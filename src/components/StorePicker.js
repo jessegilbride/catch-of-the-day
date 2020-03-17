7400b1eb36e02da6c49cc1fa5d3ff935b7761a85
Jesse Gilbride <5638513+jessegilbride@users.noreply.github.com>
2020-03-17 18:57:34 -0400

    file update
    Add functionality to allow user to go to the store entered into the form.

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import {getFunName} from '../helpers.js';
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = (event) => {
+    // stop form from submitting
+    event.preventDefault();
+    // get store name from input field
+    const storeName = this.myInput.current.value;
+    // change page to store submitted from form
+    this.props.history.push(`/store/${storeName}`);
+  }
+
   render() {
     return (
-      <form className="store-picker">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please enter a store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input type="text" ref={this.myInput} required placeholder="Store Name" defaultValue={getFunName()} />
         <button type="submit">Visit Store</button>
       </form>
     );
