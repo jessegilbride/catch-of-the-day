9eed4443b5c1f900c977b042639db48a15edbd89
Jesse Gilbride <5638513+jessegilbride@users.noreply.github.com>
2020-03-17 18:51:44 -0400

    update App, Inventory. upload AddFish.
    This update encompasses adding functionality to use a form that creates a variable which is used to set the state object in the App component.

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,29 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+  
+  addFish = (fish) => {
+    // console.log("adding a fish");
+    // 1. make a copy of existing state with the spread operator
+    const fishes = {...this.state.fishes};
+    // 2. add new fish to the fishes variable
+    fishes[`fish${Date.now()}`] = fish;
+    // 3. set the new fishes object to state
+    this.setState({fishes: fishes});
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header />
+          <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     );
   }
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import AddFishForm from './AddFishForm';
 
 class Inventory extends React.Component {
   render() {
     return (
       <div className="inventory">
-        inventory!!!
+        <AddFishForm addFish={this.props.addFish} />
       </div>
-    )
+    );
   }
 }
 
